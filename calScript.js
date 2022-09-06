f8949a15df47b8570376cf32afa09a70bfbc5e47
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2022-09-06 18:53:31 +0500

    Update calScript.js

--- calScript.js
+++ calScript.js
@@ -1,18 +1,20 @@
 class Calculator{ 
     constructor(previousOperandTextElement,currentOperandTextElement){
-        this.previousOperandTextElement=previousOperandTextElement;
-        this.currentOperandTextElement=currentOperandTextElement;
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
     }
     allClear(){
-        this.previousOperandTextElement = ' ';
-        this.currentOperandTextElement = ' ';
-        this.operation = undefined;
+        this.previousOperand = ''
+        this.currentOperand = ''
+        this.operation = undefined
     }
     delete(){
 
     }
     appendNumber(number){
-
+        if(this.currentOperand === '.' && this.currentOperand.includes('.') )return
+        
+        this.currentOperand = this.currentOperand + number;
     }
     chooseOperation(operation){
 
@@ -21,15 +23,15 @@ class Calculator{
 
     }
     updateDislay(){
-        
+        this.currentOperandTextElement.innerText = this.currentOperand;
     }
 }
 
 
 
 
-const numButton = document.querySelectorAll('[data-number]');
-const operatorButton = document.querySelectorAll('[data-operator]');
+const numButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
 const equalsButton = document.querySelector('[data-equals]');
 const acButton = document.querySelector('[data-all-clear]');
 const deleteButton = document.querySelector('[data-delete]');
@@ -37,10 +39,11 @@ const previousOperandTextElement = document.querySelector('[data-previous-operan
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 //Making an object calculator of class Calculator... 
 const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement);
+
 //Now telling buttons what to do on click
-numButton.forEach(button=>{ //for every button
-    button.addEventListener('click',()=>{ //when user clicks
-        calculator.appendNumber(button.innerHTML); //append that operand
+numButtons.forEach((button) =>{    //for every button
+    button.addEventListener('click', () => { //when user clicks
+        calculator.appendNumber(button.innerText); //append that operand
         calculator.updateDislay(); //update it to the display
     })
 }) 
