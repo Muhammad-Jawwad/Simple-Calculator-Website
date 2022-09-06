cc488f85a6db7a51bccefd994805042db764ef0b
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2022-09-06 16:31:02 +0500

    Update calScript.js

--- calScript.js
+++ calScript.js
@@ -1,10 +1,12 @@
-class Calculator{
+class Calculator{ 
     constructor(previousOperandTextElement,currentOperandTextElement){
         this.previousOperandTextElement=previousOperandTextElement;
         this.currentOperandTextElement=currentOperandTextElement;
     }
     allClear(){
-
+        this.previousOperandTextElement = ' ';
+        this.currentOperandTextElement = ' ';
+        this.operation = undefined;
     }
     delete(){
 
@@ -12,10 +14,15 @@ class Calculator{
     appendNumber(number){
 
     }
-    chooseOperator(){
+    chooseOperation(operation){
+
+    }
+    compute(){
+
+    }
+    updateDislay(){
         
     }
-
 }
 
 
@@ -28,4 +35,12 @@ const acButton = document.querySelector('[data-all-clear]');
 const deleteButton = document.querySelector('[data-delete]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
-
+//Making an object calculator of class Calculator... 
+const calculator = new Calculator(previousOperandTextElement,currentOperandTextElement);
+//Now telling buttons what to do on click
+numButton.forEach(button=>{ //for every button
+    button.addEventListener('click',()=>{ //when user clicks
+        calculator.appendNumber(button.innerHTML); //append that operand
+        calculator.updateDislay(); //update it to the display
+    })
+}) 
